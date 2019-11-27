de87e761eab0deb67572cb62d77bcf53c72b138d
Sergey Kirichenko <37999438+mangocreatorz@users.noreply.github.com>
2019-11-27 16:35:21 +0600

    Update index.js

--- CRUD MongoDB/index.js
+++ CRUD MongoDB/index.js
@@ -13,34 +13,47 @@ const PersonModel = Mongoose.model("users", {
 
 app.use(BodyParser.json());
 
-app.post("/create", /*async*/ (request, response) => {
+app.post("/create", async (request, response) => {
     try {
         var person = new PersonModel(request.body);
-        var result = /*await*/ person.save();
+        var result = await person.save();
         response.send(result);
     } catch (error) {
         response.status(500).send(error);
     }
 });
 
-app.get("/users", /*async*/ (request, response) => {
+app.get("/users", async (request, response) => {
     try {
-        var result = /*await*/ PersonModel.find().exec();
+        var result = await PersonModel.find().exec();
         response.send(result);
     } catch (error) {
         response.status(500).send(error);
     }
 });
 
-app.get("/users/:id", async (request, response) => {
+app.put("/users/:id", async (request, response) => {
     try {
         var person = await PersonModel.findById(request.params.id).exec();
-        response.send(person);
+        person.set(request.body);
+        var result = await person.save();
+        response.send(result);
     } catch (error) {
         response.status(500).send(error);
     }
 });
+
+app.delete("/users/:id", async (request, response) => {
+    try {
+        var result = await PersonModel.deleteOne({ _id: request.params.id }).exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
 
 app.listen(3000, () => {
     console.log("Listening at port 3000");
 });
+
